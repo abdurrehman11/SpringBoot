95ba4a03ae471bc49b97525e72cadc97ce718780
Abdur Rehman <rizwan.majeed@venturedive.com>
2018-10-02 23:58:22 +0500

    Spring Boot Scheduler

---
+++ SpringBoot-Schedular/src/main/java/com/infotech/SpringBootSchedular/scheduler/Scheduler.java
@@ -0,0 +1,43 @@
+package com.infotech.SpringBootSchedular.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Scheduler {
+
+//    cron expressions to schedule the execution of your tasks.
+
+    @Scheduled(cron = "5 * * * * ?")
+    public void cronJobSch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Java cron job expression:: " + strDate);
+    }
+
+//    Fixed Rate scheduler is used to execute the tasks at the specific time. It does not wait for the completion of previous task.
+
+//    @Scheduled(fixedRate = 1000)
+//    public void fixedRateSch() {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+//
+//        Date now = new Date();
+//        String strDate = sdf.format(now);
+//        System.out.println("Fixed Rate scheduler:: " + strDate);
+//    }
+
+//    Fixed Delay scheduler is used to execute the tasks at a specific time. It should wait for the previous task completion. The values should be in milliseconds.
+
+//    @Scheduled(fixedDelay = 1000, initialDelay = 3000)
+//    public void fixedDelaySch() {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+//        Date now = new Date();
+//        String strDate = sdf.format(now);
+//        System.out.println("Fixed Delay scheduler:: " + strDate);
+//    }
+
+}
